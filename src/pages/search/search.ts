05f34c0e683ebe730ff833ae82f10c86e69065fa
spazworm <spazworm@gmail.com>
2017-08-12 15:42:27 +0930

    re-arrange initial data dump

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -41,8 +41,9 @@ export class SearchPage {
     });
   }
 
-  getCounterparties() {
-    let transactions = JSON.parse(localStorage.getItem('transactions'));
-  }
+  // getCounterparties() {
+  //   let transactions = JSON.parse(localStorage.getItem('transactions'));
+    
+  // }
 
 }
