58d275de3f9b500a35fe1740fc1ec2b09f3ef4a7
Michael Drozdowski <mdrozdowski@nextfaze.com>
2017-08-12 13:57:06 +1000

    changes welcome page to loginPage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,21 @@
-import {Component, ViewChild} from '@angular/core';
-import {SplashScreen} from '@ionic-native/splash-screen';
-import {StatusBar} from '@ionic-native/status-bar';
-import {Nav, Platform} from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { Nav, Platform } from 'ionic-angular';
 
-import {CardsPage} from '../pages/cards/cards';
-import {ContentPage} from '../pages/content/content';
-import {ListMasterPage} from '../pages/list-master/list-master';
-import {LoginPage} from '../pages/login/login';
-import {MapPage} from '../pages/map/map';
-import {MenuPage} from '../pages/menu/menu';
-import {FirstRunPage} from '../pages/pages';
-import {SearchPage} from '../pages/search/search';
-import {SettingsPage} from '../pages/settings/settings';
-import {SignupPage} from '../pages/signup/signup';
-import {TabsPage} from '../pages/tabs/tabs';
-import {TutorialPage} from '../pages/tutorial/tutorial';
-import {WelcomePage} from '../pages/welcome/welcome';
+import { CardsPage } from '../pages/cards/cards';
+import { ContentPage } from '../pages/content/content';
+import { ListMasterPage } from '../pages/list-master/list-master';
+import { LoginPage } from '../pages/login/login';
+import { MapPage } from '../pages/map/map';
+import { MenuPage } from '../pages/menu/menu';
+import { FirstRunPage } from '../pages/pages';
+import { SearchPage } from '../pages/search/search';
+import { SettingsPage } from '../pages/settings/settings';
+import { SignupPage } from '../pages/signup/signup';
+import { TabsPage } from '../pages/tabs/tabs';
+import { TutorialPage } from '../pages/tutorial/tutorial';
+import { WelcomePage } from '../pages/welcome/welcome';
 
 @Component({
   template: `<ion-menu [content]="content">
@@ -37,17 +37,17 @@ import {WelcomePage} from '../pages/welcome/welcome';
   <ion-nav #content [root]="rootPage"></ion-nav>`
 })
 export class App {
-  rootPage = WelcomePage;
+  rootPage = LoginPage;
 
   @ViewChild(Nav) nav: Nav;
 
   pages: any[] = [
-    {title: 'Tutorial', component: TutorialPage}, {title: 'Welcome', component: WelcomePage},
-    {title: 'Tabs', component: TabsPage}, {title: 'Cards', component: CardsPage},
-    {title: 'Content', component: ContentPage}, {title: 'Login', component: LoginPage},
-    {title: 'Signup', component: SignupPage}, {title: 'Map', component: MapPage},
-    {title: 'Master Detail', component: ListMasterPage}, {title: 'Menu', component: MenuPage},
-    {title: 'Settings', component: SettingsPage}, {title: 'Search', component: SearchPage}
+    { title: 'Tutorial', component: TutorialPage }, { title: 'Welcome', component: WelcomePage },
+    { title: 'Tabs', component: TabsPage }, { title: 'Cards', component: CardsPage },
+    { title: 'Content', component: ContentPage }, { title: 'Login', component: LoginPage },
+    { title: 'Signup', component: SignupPage }, { title: 'Map', component: MapPage },
+    { title: 'Master Detail', component: ListMasterPage }, { title: 'Menu', component: MenuPage },
+    { title: 'Settings', component: SettingsPage }, { title: 'Search', component: SearchPage }
   ]
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
