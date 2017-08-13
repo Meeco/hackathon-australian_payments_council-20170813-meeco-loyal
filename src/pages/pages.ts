6350189e982dc1ec871f0115538c048d600ff7b7
Michael Drozdowski <mdrozdowski@nextfaze.com>
2017-08-13 12:32:02 +1000

    Change 'Map' page to "about" - changes names - adds ion card content

--- src/pages/pages.ts
+++ src/pages/pages.ts
@@ -1,3 +1,4 @@
+import { MapPage } from './map/map';
 import { ListMasterPage } from './list-master/list-master';
 import { SearchPage } from './search/search';
 import { SettingsPage } from './settings/settings';
@@ -11,6 +12,6 @@ export const MainPage = TabsPage;
 
 // The initial root pages for our tabs (remove if not using tabs)
 export const Tab1Root = ListMasterPage;
-export const Tab2Root = SearchPage;
+export const Tab2Root = MapPage;
 export const Tab3Root = SettingsPage;
 export const Tab4Root = CardsPage;
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -18,14 +18,13 @@ export class TabsPage {
   tab4Root: any = Tab4Root;
 
   tab1Title = " ";
-  tab2Title = " ";
+  tab2Title = "About";
   tab3Title = " ";
   tab4Title = " ";
 
   constructor(public navCtrl: NavController, public translateService: TranslateService) {
     translateService.get(['TAB1_TITLE', 'TAB2_TITLE', 'TAB3_TITLE', 'TAB4_TITLE']).subscribe(values => {
       this.tab1Title = values['TAB1_TITLE'];
-      this.tab2Title = values['TAB2_TITLE'];
       this.tab3Title = values['TAB3_TITLE'];
       this.tab4Title = values['TAB4_TITLE'];
     });
