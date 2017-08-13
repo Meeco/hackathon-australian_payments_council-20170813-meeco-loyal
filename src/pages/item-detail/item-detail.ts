9e88f9124d4034e5b5321cf242728cadb27ee967
Jeff Cressman <jcressman@nextfaze.com>
2017-08-13 15:05:14 +1000

    Join now button opens stuff now

--- src/pages/item-detail/item-detail.ts
+++ src/pages/item-detail/item-detail.ts
@@ -14,6 +14,7 @@ export class ItemDetailPage {
   ];
   conversion = 100;
   merchant: any = 'https://www.nab.com.au';
+  links: any[];
   offers: string[] = [
     'https://www.nab.com.au/sites/personal/credit-cards/rewards',
     'https://www.nab.com.au/sites/personal/credit-cards/rewards',
@@ -40,6 +41,7 @@ export class ItemDetailPage {
     this.total = payload.totalSpend;
     this.exists = payload.exists;
     this.merchant = payload.domain;
+    this.links = payload.links;
   }
   ngOnInit() {}
   floor(val: number) {
@@ -54,6 +56,9 @@ export class ItemDetailPage {
   }
   open(link) {
     return window.open(this.parse(link));
+  }
+  joinNow() {
+    return window.open(this.links[0]);
   }
   getDetails(id: string) {
     return this.conversions.find(({merchant}) => merchant === id);
