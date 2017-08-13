53f71b32d85650871c07498a45aed18a96970421
Paul Robinson <probinson@Mackbook-Pro-Paul-Work.local>
2017-08-13 11:38:08 +0930

    add boilerplate detail

--- src/pages/list-master/list-master.ts
+++ src/pages/list-master/list-master.ts
@@ -1,13 +1,14 @@
 import 'rxjs/add/operator/switchMap';
+
 import {Component} from '@angular/core';
-import {ModalController, NavController} from 'ionic-angular';
 import {Http} from '@angular/http';
+import {ModalController, NavController} from 'ionic-angular';
+import {combineLatest} from 'rxjs/observable/combineLatest';
+import {of } from 'rxjs/observable/of';
 
 import {Item} from '../../models/item';
 import {Items} from '../../providers/providers';
 import {ItemDetailPage} from '../item-detail/item-detail';
-import {combineLatest} from 'rxjs/observable/combineLatest';
-import {of} from 'rxjs/observable/of';
 
 function domain_from_url(url) {
   var parser = document.createElement('a');
@@ -29,82 +30,82 @@ export class ListMasterPage {
   loading: boolean = true;
 
   constructor(
-      public navCtrl: NavController, public items: Items, public modalCtrl: ModalController, public obp: OBP, public http: Http) {
-  }
+      public navCtrl: NavController, public items: Items, public modalCtrl: ModalController,
+      public obp: OBP, public http: Http) {}
 
   ngOnInit() {
     this.domains$ =
-    this.obp.api.corePrivateAccountsAllBanks()
-        .switchMap((accts: any) => {
-          return combineLatest(accts.map((acct) => {
-            return this.obp.api.accountById('owner', acct.id, acct.bank_id);
-          }));
-        })
-        .switchMap((accts: any) => {
-          let filtered =
-              accts.filter((acc) => acc.views_available.find((view) => view.id === 'owner'));
-          return combineLatest(filtered.map((acct) => {
-            return this.obp.api.getTransactionsForBankAccount('owner', acct.id, acct.bank_id)
-                .map((txs) => {
-                  if (txs && txs.transactions.length) {
-                    this.message = `Found ${
-                                            this.txcount += txs.transactions.length
-                                          } Transactions for Lost Rewards!`;
-                  }
-                  return txs;
-                });
-          }));
-        })
-        .map(transactions => {
+        this.obp.api.corePrivateAccountsAllBanks()
+            .switchMap((accts: any) => {
+              return combineLatest(accts.map((acct) => {
+                return this.obp.api.accountById('owner', acct.id, acct.bank_id);
+              }));
+            })
+            .switchMap((accts: any) => {
+              let filtered =
+                  accts.filter((acc) => acc.views_available.find((view) => view.id === 'owner'));
+              return combineLatest(filtered.map((acct) => {
+                return this.obp.api.getTransactionsForBankAccount('owner', acct.id, acct.bank_id)
+                    .map((txs) => {
+                      if (txs && txs.transactions.length) {
+                        this.message = `Found ${
+                                                this.txcount += txs.transactions.length
+                                              } Transactions for Lost Rewards!`;
+                      }
+                      return txs;
+                    });
+              }));
+            })
+            .map(transactions => {
 
-          let txs =
-              transactions.map(({transactions}) => transactions).reduce((a, b) => a.concat(b));
-          txs = txs.reduce((a, b) => {
-            a[b.other_account.metadata.URL] = [...a[b.other_account.metadata.URL] || [], b];
-            return a;
-          }, {});
-          return Object.keys(txs);
-        })
-        .switchMap((urls) => {
-          this.count = urls.length;
-          this.message = `Found ${this.count} Potential Reward Sources`;
-          return combineLatest(urls.filter((url) => url !== 'null').map((url) => {
-            let parsed = <any>domain_from_url(url);
-            return this.http.get(parsed.origin)
-                .catch(() => of ({
-                         text: function() {
-                           return '';
-                         }
-                       }))
-                .map((res) => {
-                  this.message = `${--this.count - 1} Potential Reward Sources Remain`;
-                  return res.text();
-                })
-                .filter((val) => !!val)
-                .map((str) => {
-                  let a = str.match(/href="([^\'\"]+)/g);
-                  if (a === null) {
-                    return [parsed.origin, []];
-                  }
-                  let b = a.map((val) => val.slice(6))
-                              .filter(
-                                  (str) => str.includes('rewards') || str.includes('reward') ||
-                                      str.includes('loyalty') || str.includes('membership'));
-                  return [parsed.origin, b || []];
-                });
-          }));
-        })
-        .map((payload) => {
-          let count = payload
-                          .map((pay) => {
-                            let count = pay[1].length;
-                            return count;
-                          })
-                          .reduce((a, b) => (a + b), 0);
-          this.message = `Found ${count} Unclaimed Reward Sources`;
-          this.loading = false;
-          return payload;
-        });    
+              let txs =
+                  transactions.map(({transactions}) => transactions).reduce((a, b) => a.concat(b));
+              txs = txs.reduce((a, b) => {
+                a[b.other_account.metadata.URL] = [...a[b.other_account.metadata.URL] || [], b];
+                return a;
+              }, {});
+              return Object.keys(txs);
+            })
+            .switchMap((urls) => {
+              this.count = urls.length;
+              this.message = `Found ${this.count} Potential Reward Sources`;
+              return combineLatest(urls.filter((url) => url !== 'null').map((url) => {
+                let parsed = <any>domain_from_url(url);
+                return this.http.get(parsed.origin)
+                    .catch(() => of ({
+                             text: function() {
+                               return '';
+                             }
+                           }))
+                    .map((res) => {
+                      this.message = `${--this.count - 1} Potential Reward Sources Remain`;
+                      return res.text();
+                    })
+                    .filter((val) => !!val)
+                    .map((str) => {
+                      let a = str.match(/href="([^\'\"]+)/g);
+                      if (a === null) {
+                        return [parsed.origin, []];
+                      }
+                      let b = a.map((val) => val.slice(6))
+                                  .filter(
+                                      (str) => str.includes('rewards') || str.includes('reward') ||
+                                          str.includes('loyalty') || str.includes('membership'));
+                      return [parsed.origin, b || []];
+                    });
+              }));
+            })
+            .map((payload) => {
+              let count = payload
+                              .map((pay) => {
+                                let count = pay[1].length;
+                                return count;
+                              })
+                              .reduce((a, b) => (a + b), 0);
+              this.message = `Found ${count} Unclaimed Reward Sources`;
+              this.loading = false;
+              return payload;
+            });
   }
 
   /**
@@ -112,12 +113,12 @@ export class ListMasterPage {
    */
   ionViewDidLoad() {}
 
-  total(vals: any[]) {
-    return vals.reduce((a, b) => {
-      let c = a + +b.details.value.amount;
-      return c;
-    }, 0)
-  }
+  total(vals: any[]){return vals.reduce(
+      (a, b) => {
+        let c = a + +b.details.value.amount;
+        return c;
+      },
+      0)}
 
   /**
    * Navigate to the detail page for this item.
@@ -128,16 +129,8 @@ export class ListMasterPage {
 
   // TMP
   open(domain: string, links: string[]) {
-    links.forEach((link) => {
-      // console.log(link);
-      if (link[0] === '/') {
-        window.open(domain + link);
-        return;
-      } else {
-        window.open(link, '_blank');
-      }
-    });
-  }  
+    this.navCtrl.push(ItemDetailPage, {domain, links});
+  }
 
   avatar(short_name: string) {
     return `https://api.adorable.io/avatars/285/${short_name}.png`
