4b5068e477a3a8975212f67536814c85f405063f
Jeff Cressman <jcressman@nextfaze.com>
2017-08-13 11:59:25 +1000

    Start of master page

--- src/pages/list-master/list-master.ts
+++ src/pages/list-master/list-master.ts
@@ -1,19 +1,110 @@
+import 'rxjs/add/operator/switchMap';
 import {Component} from '@angular/core';
 import {ModalController, NavController} from 'ionic-angular';
-import {Observable} from 'rxjs/Observable';
+import {Http} from '@angular/http';
 
 import {Item} from '../../models/item';
 import {Items} from '../../providers/providers';
-import {ItemCreatePage} from '../item-create/item-create';
 import {ItemDetailPage} from '../item-detail/item-detail';
+import {combineLatest} from 'rxjs/observable/combineLatest';
+import {of} from 'rxjs/observable/of';
+
+function domain_from_url(url) {
+  var parser = document.createElement('a');
+  parser.href = url;
+  return parser;
+}
+
+import {OBP} from '../../providers/obp';
 
 @Component({selector: 'page-list-master', templateUrl: 'list-master.html'})
 export class ListMasterPage {
-  currentItems$: any;
+  domains$: any;
+  data: any = {};
+  localData: any = {};
+  count = 0;
+  txcount = 0;
+  transactions$: any;
+  message: string;
+  loading: boolean = true;
 
   constructor(
-      public navCtrl: NavController, public items: Items, public modalCtrl: ModalController) {
-    this.currentItems$ = this.items.query({id: 'au.01.aum.anz'});
+      public navCtrl: NavController, public items: Items, public modalCtrl: ModalController, public obp: OBP, public http: Http) {
+  }
+
+  ngOnInit() {
+    this.domains$ =
+    this.obp.api.corePrivateAccountsAllBanks()
+        .switchMap((accts: any) => {
+          return combineLatest(accts.map((acct) => {
+            return this.obp.api.accountById('owner', acct.id, acct.bank_id);
+          }));
+        })
+        .switchMap((accts: any) => {
+          let filtered =
+              accts.filter((acc) => acc.views_available.find((view) => view.id === 'owner'));
+          return combineLatest(filtered.map((acct) => {
+            return this.obp.api.getTransactionsForBankAccount('owner', acct.id, acct.bank_id)
+                .map((txs) => {
+                  if (txs && txs.transactions.length) {
+                    this.message = `Found ${
+                                            this.txcount += txs.transactions.length
+                                          } Transactions for Lost Rewards!`;
+                  }
+                  return txs;
+                });
+          }));
+        })
+        .map(transactions => {
+
+          let txs =
+              transactions.map(({transactions}) => transactions).reduce((a, b) => a.concat(b));
+          txs = txs.reduce((a, b) => {
+            a[b.other_account.metadata.URL] = [...a[b.other_account.metadata.URL] || [], b];
+            return a;
+          }, {});
+          return Object.keys(txs);
+        })
+        .switchMap((urls) => {
+          this.count = urls.length;
+          this.message = `Found ${this.count} Potential Reward Sources`;
+          return combineLatest(urls.filter((url) => url !== 'null').map((url) => {
+            let parsed = <any>domain_from_url(url);
+            return this.http.get(parsed.origin)
+                .catch(() => of ({
+                         text: function() {
+                           return '';
+                         }
+                       }))
+                .map((res) => {
+                  this.message = `${--this.count - 1} Potential Reward Sources Remain`;
+                  return res.text();
+                })
+                .filter((val) => !!val)
+                .map((str) => {
+                  let a = str.match(/href="([^\'\"]+)/g);
+                  if (a === null) {
+                    return [parsed.origin, []];
+                  }
+                  let b = a.map((val) => val.slice(6))
+                              .filter(
+                                  (str) => str.includes('rewards') || str.includes('reward') ||
+                                      str.includes('loyalty') || str.includes('membership'));
+                  return [parsed.origin, b || []];
+                });
+          }));
+        })
+        .map((payload) => {
+          let count = payload
+                          .map((pay) => {
+                            let count = pay[1].length;
+                            return count;
+                          })
+                          .reduce((a, b) => (a + b), 0);
+          this.message = `Found ${count} Unclaimed Reward Sources`;
+          this.loading = false;
+          return payload;
+        });    
   }
 
   /**
@@ -21,25 +112,11 @@ export class ListMasterPage {
    */
   ionViewDidLoad() {}
 
-  /**
-   * Prompt the user to add a new item. This shows our ItemCreatePage in a
-   * modal and then adds the new item to our data source if the user created one.
-   */
-  addItem() {
-    let addModal = this.modalCtrl.create(ItemCreatePage);
-    addModal.onDidDismiss(item => {
-      if (item) {
-        this.items.add(item);
-      }
-    })
-    addModal.present();
-  }
-
-  /**
-   * Delete an item from the list of items.
-   */
-  deleteItem(item) {
-    this.items.delete(item);
+  total(vals: any[]) {
+    return vals.reduce((a, b) => {
+      let c = a + +b.details.value.amount;
+      return c;
+    }, 0)
   }
 
   /**
@@ -48,6 +125,20 @@ export class ListMasterPage {
   openItem(item: Item) {
     this.navCtrl.push(ItemDetailPage, {item: item});
   }
+
+  // TMP
+  open(domain: string, links: string[]) {
+    links.forEach((link) => {
+      // console.log(link);
+      if (link[0] === '/') {
+        window.open(domain + link);
+        return;
+      } else {
+        window.open(link, '_blank');
+      }
+    });
+  }  
+
   avatar(short_name: string) {
     return `https://api.adorable.io/avatars/285/${short_name}.png`
   }
